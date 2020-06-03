8e6af8b8f9c1f389fa51ec46b89f2d0cf7570c5c
Kevin Marin <45517485+marink2@users.noreply.github.com>
2020-06-03 12:48:55 -0400

    Molar volume function tested. Used numpy.allclose.

--- aqpolypy/unit_tests/waterMilleroAW_unit_test.py
+++ aqpolypy/unit_tests/waterMilleroAW_unit_test.py
@@ -1,26 +1,34 @@
 """
 Unit Test for WaterPropertiesFineMillero
 """
+import numpy as np
 import unittest
 import aqpolypy.water.WaterPropertiesFineMillero as fm
 
 
 class TestWaterMilleroAW(unittest.TestCase):
 
-    # Testing Boltzman constant
+    # Testing density (Fine Millero)
     def test_density(self):
-        # density of water at 25 C from Millero paper (1.012107 = specific volume)
+        # density of water at 25 C (298.15 K) from Millero paper (1.002961 = specific volume)
         measured_density = (1 / 1.002961) * 1000
-        # testing density
+        # testing density up to a precision of 10^-6
         wfm = fm.WaterPropertiesFineMillero(298.15)
-        self.assertEqual(wfm.density(), measured_density)
+        np.allclose(wfm.density(), measured_density, 0, 1e-6)
 
+    # Testing molar volume (Fine Millero)
     def test_molar_volume(self):
-        pass
+        # molar volume of water at 0 C (273.15); specific volume * molecular weight
+        measured_molarVolume = (1.000160 / 1e6) * 18.01534
+        # testing molar volume with specific volume = 1.000160 cm^3/g
+        wfm = fm.WaterPropertiesFineMillero(273.15)
+        np.allclose(wfm.molar_volume(), measured_molarVolume, 0, 1e-6)
 
+    # Testing dielectric constant (Archer Wang)
     def test_dielectric_constant(self):
         pass
 
+    # Testing compressibility (Fine Millero)
     def test_compressibility(self):
         pass
 
