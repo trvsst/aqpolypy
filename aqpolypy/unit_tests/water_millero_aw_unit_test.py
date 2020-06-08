ff3b22b350cf2b9cfabbdddf99c526426381690c
Kevin Marin <45517485+marink2@users.noreply.github.com>
2020-06-08 02:55:03 -0400

    Unit test proceeds with no errors. Individual tests are passed.

--- aqpolypy/unit_tests/water_millero_aw_unit_test.py
+++ aqpolypy/unit_tests/water_millero_aw_unit_test.py
@@ -1,5 +1,15 @@
 """
-Unit Test for WaterPropertiesFineMillero
+:module: water_millero_aw_unit_test
+:platform: Unix, Windows, OS
+:synopsis: unit test for WaterMilleroAW
+
+.. moduleauthor:: Alex Travesset <trvsst@ameslab.gov>, May2020
+.. history::
+..                Kevin Marin <marink2@tcnj.edu>, May2020
+..                  - Created member methods to test each member method of WaterMilleroAW class.
+..                  - Tested single value cases utilizing assertEquals functions.
+..                  - Implemented allclose function to limit number of decimal places checked.
+..                  - Arrays were added to test multiple values.
 """
 import numpy as np
 import unittest
@@ -14,15 +24,14 @@ class TestWaterMilleroAW(unittest.TestCase):
         param = np.array([[5, 1, 1.000036],
                           [30, 1, 1.004369],
                           [75, 1, 1.025805],
-                          [35, 100, 1.001597],
+                          [30, 100, 0.999939],
                           [55, 300, 1.001642]])
-        # converting param to [temperature(K), pressure(atm), density]
+        # converting param to [temperature(K), pressure(atm), specific volume]
         param[:, 0] = 273.15 + param[:, 0]
         param[:, 1] = param[:, 1] / 1.01325
-        param[:, 2] = (1 / param[:, 2]) * 1000
-        # testing density up to a precision of 10^-6
+        # testing density up to a precision of 10^-4
         wfm = fm.WaterPropertiesFineMillero(param[:, 0], param[:, 1])
-        test_vals = np.allclose(wfm.density(), param[:, 2], 0, 1e-6)
+        test_vals = np.allclose(1e3 / wfm.density(), param[:, 2], 0, 1e-4)
         self.assertTrue(test_vals)
 
     # Testing molar volume (Fine Millero)
@@ -36,7 +45,7 @@ class TestWaterMilleroAW(unittest.TestCase):
         # converting param to [temperature(K), pressure(atm), molar volume]
         param[:, 0] = 273.15 + param[:, 0]
         param[:, 1] = param[:, 1] / 1.01325
-        param[:, 2] = (param[:, 2] / 1e6) * fm.WaterPropertiesFineMillero(0).MolecularWeight
+        param[:, 2] = (param[:, 2] / 1e6) * fm.WaterPropertiesFineMillero(300).MolecularWeight
         # testing molar volume up to a precision of 10^-6
         wfm = fm.WaterPropertiesFineMillero(param[:, 0], param[:, 1])
         test_vals = np.allclose(wfm.molar_volume(), param[:, 2], 0, 1e-6)
@@ -49,13 +58,13 @@ class TestWaterMilleroAW(unittest.TestCase):
                           [318.15, 0.1, 71.50],
                           [353.15, 0.1, 60.87],
                           [273.15, 1, 87.94],
-                          [283.15, 10, 84.33]])
+                          [278.15, 60, 88.20]])
         # converting param to [temperature(K), pressure(atm), dielectric constant]
         param[:, 1] = param[:, 1] / 0.101325
-        # testing dielectric constant up to a precision of 10^-6
+        # testing dielectric constant up to a precision of 10^-2
         wfm = fm.WaterPropertiesFineMillero(param[:, 0], param[:, 1])
-        test_vals = np.allclose(wfm.dielectric_constant(), param[:, 2], 0, 1e-6)
-        self.assertTrue(test_vals)
+        test_vals = np.allclose(wfm.dielectric_constant(), param[:, 2], 0, 1e-2)
+        self.assertTrue(test_vals, wfm.dielectric_constant())
 
     # Testing compressibility (Fine Millero)
     def test_compressibility(self):
@@ -68,7 +77,7 @@ class TestWaterMilleroAW(unittest.TestCase):
         # converting param to [temperature(K), pressure(atm), compressibility (atm-1)]
         param[:, 0] = 273.15 + param[:, 0]
         param[:, 1] = param[:, 1] / 1.01325
-        param[:, 2] = (param[:, 2] * 1e6) * 1.01325
+        param[:, 2] = (param[:, 2] / 1e6) * 1.01325
         # testing compressibility up to a precision of 10^-6
         wfm = fm.WaterPropertiesFineMillero(param[:, 0], param[:, 1])
         test_vals = np.allclose(wfm.compressibility(), param[:, 2], 0, 1e-6)
