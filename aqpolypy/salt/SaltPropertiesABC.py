4b855c53a26bdda2697f9ba34bd5cb6bcbfecc8e
Kevin Marin <45517485+marink2@users.noreply.github.com>
2020-06-20 18:03:24 -0400

    Added symbol for molar volume infinite dilution.

--- aqpolypy/salt/SaltPropertiesABC.py
+++ aqpolypy/salt/SaltPropertiesABC.py
@@ -43,7 +43,8 @@ class SaltProperties(ABC):
     @abstractmethod
     def molar_vol_infinite_dilution(self):
         """
-        Abstract method: calculates the partial molal volume of solute at infinite dilution :math:
+        Abstract method: calculates the partial molal volume of solute at infinite dilution :math:`\\bar{\\upsilon}^{
+                                                                                                    \\circ}_s`
         """
         pass
 
