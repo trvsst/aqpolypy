ccba3d8b8ba0f54712d60e5bddf0c9b58d11721a
Kevin Marin <45517485+marink2@users.noreply.github.com>
2020-06-29 08:26:15 -0400

    Added BP calculations for dielectric constant derivative w/ temperature. Added millero calculations for thermal expansion (a_w) and enthalpy coefficient (A_H). In density calculations: changed self.y = un.atm_2_bar(self.pa - 1) -> self.y = un.atm_2_bar(self.pa) for greater accuracy. Note: a condition where 1 atm = 0 bar needs to be added.

--- aqpolypy/water/WaterMilleroBP.py
+++ aqpolypy/water/WaterMilleroBP.py
@@ -40,7 +40,7 @@ class WaterPropertiesFineMillero(wp.WaterProperties):
         Calculations for density
         """
         self.t = self.tk - un.celsius_2_kelvin(0)
-        self.y = un.atm_2_bar((self.pa - 1))
+        self.y = un.atm_2_bar(self.pa)
 
         self.den1 = 0.9998396 + self.t * 18.224944e-3 - 7.922210e-6 * self.t ** 2 - 55.44846e-9 * self.t ** 3
         self.den2 = 149.7562e-12 * self.t ** 4 - 393.2952e-15 * self.t ** 5
@@ -80,7 +80,17 @@ class WaterPropertiesFineMillero(wp.WaterProperties):
         self.B_dc = self.U[6] + (self.U[7] / self.tk) + self.U[8] * self.tk
 
         self.dielectricConstant = self.D100 + self.C * np.log((self.B_dc + un.atm_2_bar(self.pa)) / (self.B_dc + 1000))
-        self.dielectricConstant_der = un.atm_2_bar(1) * self.C / (self.B_dc + (un.atm_2_bar(self.pa)))
+
+        # dielectric constant derivative with respect to pressure
+        self.dielectricConstant_der_p = un.atm_2_bar(1) * self.C / (self.B_dc + (un.atm_2_bar(self.pa)))
+
+        # dielectric constant derivative with respect to temperature
+        self.val_1 = (un.atm_2_bar(self.pa) - 1000) * (self.U[3] * (self.U[5] + self.tk) + self.U[4]) * (self.U[8] * self.tk ** 2 - self.U[7])
+        self.val_2 = (self.U[5] + self.tk) * (self.U[6] * self.tk + self.tk * (self.U[8] * self.tk + 1000) + self.U[7]) * (self.tk * (un.atm_2_bar(self.pa) + self.U[8] * self.tk) + self.U[6] * self.tk + self.U[7])
+        self.val_3 = (self.U[4] * np.log((un.atm_2_bar(self.pa) + (self.U[7] / self.tk) + self.U[8] * self.tk + self.U[6]) / (1000 + (self.U[7] / self.tk) + self.U[8] * self.tk + self.U[6]))) / ((self.U[5] + self.tk) ** 2)
+        self.val_4 = (self.U[0] * np.exp(self.U[1] * self.tk + self.U[2] * self.tk ** 2)) * (2 * self.U[2] * self.tk + self.U[1])
+
+        self.dielectricConstant_der_t = -(self.val_1 / self.val_2) - self.val_3 + self.val_4
 
         #  Calculations for compressibility
 
@@ -102,10 +112,33 @@ class WaterPropertiesFineMillero(wp.WaterProperties):
         # add a factor 1e6 and convert to Pascal (lat two parts of a_v_0)
         self.a_v_0 = 2 * self.osmotic_coefficient * un.r_gas() * self.tk * 1e6 / un.atm_2_pascal(1)
         self.a_v_1 = self.dielectricConstant
-        self.a_v_2 = 3 * self.dielectricConstant_der
+        self.a_v_2 = 3 * self.dielectricConstant_der_p
         self.a_v_3 = self.comp
 
         self.app_molal_vol = self.a_v_0 * (self.a_v_2 / self.a_v_1 - self.a_v_3)
+
+        # Calculations for enthalpy coefficient
+
+        # thermal expansion coefficient
+        self.V0_der_1 = 2.85685e-14 * self.t ** 5 - 6.19212e-12 * self.t ** 4 + 1.41483e-9 * self.t ** 3
+        self.V0_der_2 = 3.10211e-7 * self.t ** 2 + 0.0000158444 * self.t - 0.0000681318
+        self.V0_der_3 = -3.93295e-13 * self.t ** 5 + 1.49756e-10 * self.t ** 4 - 5.54485e-8 * self.t ** 3
+        self.V0_der_4 = -7.92221e-6 * self.t ** 2 + 0.0182249 * self.t + 0.99984
+
+        self.V0_der = (self.V0_der_1 + self.V0_der_2) / ((self.V0_der_3 + self.V0_der_4) ** 2)
+
+        self.B_der = -0.000091156 * self.t ** 3 + 0.031434 * self.t ** 2 - 4.43108 * self.t + 147.037
+        self.a1_der = 3.3908e-8 * self.t ** 3 - 8.46e-6 * self.t ** 2 + 0.00056892 * self.t - 0.002391
+        self.a2_der = -1.3196e-11 * self.t ** 3 + 2.3826e-9 * self.t ** 2 - 6.998e-8 * self.t - 3.913e-6
+
+        self.aw_term_1 = (1 / self.vol) * self.V0_der
+        self.aw_term_2 = -((self.y * self.V0_der) / (self.vol * (self.B + self.a1 * self.y + self.a2 * self.y ** 2)))
+        self.aw_term_3 = self.y * self.V0 * ((self.B_der + self.y * self.a1_der + self.a2_der * self.y ** 2) / (self.vol * (self.B + self.a1 * self.y + self.a2 * self.y ** 2) ** 2))
+
+        self.aw = self.aw_term_1 + self.aw_term_2 + self.aw_term_3
+
+        # enthalpy coefficient
+        self.a_h = -6 * self.osmotic_coefficient * un.r_gas() * self.tk * (1 + self.tk * (1 / self.dielectricConstant) * self.dielectricConstant_der_t + tk * (self.aw / 3))
 
     def density(self):
         """
@@ -175,3 +208,12 @@ class WaterPropertiesFineMillero(wp.WaterProperties):
             :rtype: float
             """
         return self.app_molal_vol
+
+    def enthalpy_coefficient(self):
+        """
+            Enthalpy coefficient according to Bradley and Pitzer :cite:`Bradley1979`
+
+            :return: enthalpy coefficient in SI
+            :rtype: float
+            """
+        return self.a_h
