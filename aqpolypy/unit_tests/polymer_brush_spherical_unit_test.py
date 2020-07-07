47d76906c96715f8b14514a91f8fe0ed26bdc9ea
Travesset-Casas <trvsst@iastate.edu>
2020-07-07 10:22:26 -0500

    Fixed unit test

--- aqpolypy/unit_tests/polymer_brush_spherical_unit_test.py
+++ aqpolypy/unit_tests/polymer_brush_spherical_unit_test.py
@@ -61,7 +61,7 @@ class TestPolymerFreeSpherical(unittest.TestCase):
         b_sph = Bb.BinaryBrush(dim, chi, sigma, rad, pl_peo, lag)
 
         u_mat = np.array([0.0, 0.5, 1.5])
-        res_val = np.array([[0.999984195, -2.718e-08], [0.9999840270, -2.398e-08], [0.999983827, -2.049e-08]])
+        res_val = np.array([[0.999983934, -2.3895e-08], [0.999983763, -2.0928e-08], [0.999983560, -1.771e-08]])
         comp_phi = np.zeros([u_mat.shape[0], 2])
         for ind, u_val in enumerate(u_mat):
             phi_s = b_sph.phi(u_val)
@@ -83,10 +83,9 @@ class TestPolymerFreeSpherical(unittest.TestCase):
 
         v_mat = np.array([0.0, 0.5, 1.5, 4.5, 8.5, 11.75])
         res_val = np.zeros([v_mat.shape[0], 2])
-        res_val[:, 0] = [0.5575667595709363, 0.5327362189194406, 0.49330648204453487, 0.4262814916316443,
-                         0.39285383548929875, 0.38292354454074434]
-        res_val[:, 1] = [-1.5543122344752192e-15, -4.163336342344337e-17, 4.163336342344337e-17,
-                         -2.6020852139652106e-16, - 3.4867941867133823e-16, -6.546586189815073e-14]
+        res_val[:, 0] = [0.54512457, 0.51832402, 0.47476192, 0.39479913, 0.34697209, 0.32967486]
+        res_val[:, 1] = [-5.55111512e-17,  0.00000000e+00, -2.77555756e-17,  5.20417043e-17, -1.29445066e-14,
+                         -1.14491749e-16]
         c_phi = np.zeros([v_mat.shape[0], 2])
         for ind, u_val in enumerate(v_mat):
             phi_s = b_sph.phi(u_val)
@@ -94,7 +93,7 @@ class TestPolymerFreeSpherical(unittest.TestCase):
             c_phi[ind, 0] = phi_s
             c_phi[ind, 1] = eqn
 
-        t_mat = [2e-16, 2.0e-16]
+        t_mat = [2e-8, 2.0e-16]
         for ind in range(2):
             exp1 = np.allclose(res_val[:, ind], c_phi[:, ind], atol=t_mat[ind], rtol=0.0)
             self.assertTrue(exp1)
@@ -126,15 +125,15 @@ class TestPolymerFreeSpherical(unittest.TestCase):
         b_sph = Bb.BinaryBrush(dim, chi, sigma, rad, pl_peo, lag)
         h_val = b_sph.determine_h()
         fr = b_sph.free_energy()
-        cp_vals = np.array([h_val, fr[0]])
-        ac_vals = np.array([11.750745890704016, 145.65149475350745])
+        cp_vals = np.array([h_val, fr[0][0], fr[1][0], fr[2][0]])
+        ac_vals = np.array([12.4543068094379, 52.25261599, 93.88085416, 5.08157019])
         exp = np.allclose(cp_vals, ac_vals, atol=1e-13)
         self.assertTrue(exp)
 
         # compute optimal brush size
         res = b_sph.optimal_lambda()
-        res_comp = 0.03264222743670685 - b_sph.chi
-        fun_comp = 145.06444193602587
+        res_comp = -0.5929852656156657
+        fun_comp = 151.18609253170362
         res_diff = np.abs(res_comp - res.x)
         fun_diff = np.abs(fun_comp-res.fun)
         self.assertLess(res_diff, 1e-8)
@@ -143,32 +142,39 @@ class TestPolymerFreeSpherical(unittest.TestCase):
         # compare free energies, brush size, optimal Lambda and free energy derivative
         delta_res = 0.97 * np.abs(res.x+b_sph.chi)
         vals = res.x + np.linspace(-delta_res, delta_res, 11)
-        free_energy = np.zeros_like(vals)
+        free_energy = np.zeros([vals.shape[0], 3])
         h_vals = np.zeros_like(vals)
         d_vals = np.zeros_like(vals)
-        f_comp = np.array([145.65211862, 145.47157141, 145.31436453, 145.18690686, 145.09864659,
-                           145.06444194, 145.10980527, 145.28395308, 145.69043856, 146.46990222, 147.57747715])
-        h_comp = np.array([11.75017423, 11.93516383, 12.14449686, 12.38515022, 12.66761464, 13.00871698, 13.43784993,
-                           14.01204153, 14.84611594, 16.05257333, 17.48202434])
-        l_comp = np.array([0.00097927, 0.00731186, 0.01364445, 0.01997704, 0.02630964, 0.03264223, 0.03897482,
-                           0.04530741, 0.05164, 0.0579726, 0.06430519])
-        d_comp = np.array([-1.0916966246944142, -0.8652269502955683, -0.6422216779856562, -0.4231721404736284,
-                           -0.20876074766049868, -1.0557409524381001e-07, 0.20145925688545674, 0.3925406913593079,
-                           0.5666507519715793, 0.7131280432656801, 0.8312009645838874])
+        f_comp = np.array([[52.57338855, 93.5907969, 5.05843421], [52.01845303, 94.09298618, 5.09861057],
+                           [51.45344121, 94.60620618, 5.14026642], [50.87778347, 95.13109749, 5.18351718],
+                           [50.29085432, 95.6683648, 5.2284931 ], [49.69196466, 96.21878595, 5.27534192],
+                           [49.08035284, 96.78322257, 5.32423227], [48.45517411, 97.36263262, 5.37535786],
+                           [47.81548843, 97.95808509, 5.42894285], [47.16024629, 98.5707773, 5.48524865],
+                           [46.48827252, 99.20205492, 5.54458278]])
+        h_comp = np.array([12.41978285, 12.47976726, 12.54212312, 12.60704343, 12.67474614, 12.74547861, 12.81952321,
+                           12.89720425, 12.97889664, 13.06503691, 13.15613726])
+        l_comp = np.array([-0.59978956, -0.5984287,  -0.59706784, -0.59570698, -0.59434612, -0.59298527, -0.59162441,
+                           -0.59026355, -0.58890269, -0.58754183, -0.58618097])
+        d_comp = np.array([-2.33270867e-01, -1.86167508e-01, -1.39278773e-01, -9.26141030e-02, -4.61839588e-02,
+                           6.57251675e-09, 4.59247282e-02, 9.15754564e-02, 1.36935411e-01,
+                           1.81985337e-01, 2.26702928e-01])
+
         for ind, lam_lg in enumerate(vals):
             fe_b = Bb.BinaryBrush(dim, chi, sigma, rad, pl_peo, lam_lg)
             h_calc = fe_b.determine_h()
             # brush size
             h_vals[ind] = h_calc
             # free energy
-            free_energy[ind] = fe_b.free_energy()[0]
+            fr = fe_b.free_energy()
+            for ind_p in range(3):
+                free_energy[ind, ind_p] = fr[ind_p][0]
             # free energy derivative
             d_vals[ind] = fe_b.der_free_energy()
 
-        self.assertTrue(np.allclose(f_comp, free_energy))
         self.assertTrue(np.allclose(h_vals, h_comp))
-        self.assertTrue(np.allclose(l_comp-chi, vals))
+        self.assertTrue(np.allclose(l_comp, vals))
         self.assertTrue(np.allclose(d_comp, d_vals, atol=1e-6, rtol=0.0))
+        self.assertTrue(np.allclose(f_comp, free_energy))
 
     def test_solveh(self):
         """testing solving for the lagrange parameter that gives a brush size"""
