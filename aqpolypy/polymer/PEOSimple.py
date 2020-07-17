361af4ecfb2350f247932bb75614f25488d8d91f
Travesset-Casas <trvsst@iastate.edu>
2020-07-17 17:11:59 -0500

    added hydrogen bond parameters for PEO

--- aqpolypy/polymer/PEOSimple.py
+++ aqpolypy/polymer/PEOSimple.py
@@ -7,7 +7,7 @@
 .. history:
 ..
 """
-
+import numpy as np
 import aqpolypy.polymer.PolymerPropertiesABC as pa
 
 
@@ -29,6 +29,11 @@ class PEOSimple(pa.PolymerProperties):
 
         super().__init__(mw, m_unit, m_length, k_length, nu)
 
+        self.egy = self.hydrogen_bond_energy()
+        self.enty = self.hydrogen_bond_entropy()
+        self.ang = 0.0
+        self.d_f = 0.0
+
         self.p_name = 'Polyethylene Oxide'
 
     def name(self):
@@ -39,3 +44,43 @@ class PEOSimple(pa.PolymerProperties):
         """
 
         return self.p_name
+
+    def hydrogen_bond_energy(self, egy=1800):
+        """
+        Change in internal energy :math:`\\Delta E= E_{water} -  E_{peo}`
+
+        :param egy: Internal energy (default is 1800 K)
+
+        :return: Change in internal energy (in temperature units)
+        """
+
+        self.egy = egy
+
+        return egy
+
+    def hydrogen_bond_entropy(self, ang= np.pi/5.5):
+        """
+        Change in internal energy :math:`\\Delta S= S_{water} -  S_{peo}`
+
+        :param ang: angle parameterizing the entropy (default is :math:`\\frac{5\\pi}{11)
+        :return: Change in internal energy (in temperature units)
+        """
+
+        self.ang = ang
+
+        self.enty = -np.log(0.5*(1-np.cos(ang)))
+
+        return self.enty
+
+    def hydrogen_bond_free_energy(self, temp):
+        """
+
+        Change in Free energy :math:`\\frac{\\Delta F}{k_BT}= \\frac{\\Delta E}{k_B T}-\\frac{\\Delta S}{k_B}`
+
+        :param temp: Temperature in K
+        :return: change in free energy
+        """
+
+        self.d_f = self.egy/temp - self.enty
+
+        return self.df
