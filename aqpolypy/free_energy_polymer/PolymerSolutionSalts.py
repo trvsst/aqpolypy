f6535b4e58a80dc4208a1c090398d39d790aa33a
ychi <ychi@iastate.edu>
2020-08-04 15:20:56 -0500

    This file calculates the free energy

--- aqpolypy/free_energy_polymer/PolymerSolutionSalts.py
+++ aqpolypy/free_energy_polymer/PolymerSolutionSalts.py
@@ -9,7 +9,7 @@
 """
 import numpy as np
 from scipy.special import xlogy as lg
-from scipy.optimize import fsolve
+from scipy.special import gamma as gamma
 
 import aqpolypy.salts_theory.DebyeHuckel as dh
 import aqpolypy.salts_theory.HardCore as hc
@@ -20,21 +20,21 @@ class PolymerSolutionSalts(object):
     Defines a solution with polymers with hydrogen bonds following the model :cite:`Dormidontova2002`
     """
 
-    def __init__(self, phi_p, phi_a, phi_b, x, p, n_kuhn, u_p, u_a, u_b, f_a, f_b, chi_p, chi_e, df_w, df_p, df_a, df_b, h_a, h_b):
+    def __init__(self, phi_p, phi_a, phi_b, x, p, n_kuhn, u_p, u_a, u_b, f_a, f_b, chi_p, chi_e, df_w, df_p, df_a, df_b, h_a, h_b, bjerrum_object, b_fac=np.array([1, 1, 0])):
         """
         The constructor
 
         :param phi_p: polymer fraction :math:`\\phi_p`
-        :param phi_a: salt fraction :math:`\\phi_a`
-        :param phi_b: salt fraction :math:`\\phi_b`
+        :param phi_a: salt fraction :math:`\\phi_+`
+        :param phi_b: salt fraction :math:`\\phi_-`
         :param x_ini: fraction of polymer hydrogen bonds
         :param p_ini: fraction of water hydrogen bonds
         :param n_kuhn: number of Kuhn lengths
         :param u_p: ratio of solvent to polymer volume fraction :math:`\\frac{\\upsilon_w}{\\upsilon_p}`
         :param u_a: ratio of salt to polymer volume fraction :math:`\\frac{\\upsilon_w}{\\upsilon_+}`
         :param u_b: ratio of salt to polymer volume fraction :math:`\\frac{\\upsilon_w}{\\upsilon_-}`
-        :param f_a: math:`h_a \\frac{\\upsilon_w}{\\upsilon_+} \\frac{\\phi_+}{\\( 1 - self.phi_p - self.phi_a - self.phi_b )}`
-        :param f_b: math:`h_b \\frac{\\upsilon_w}{\\upsilon_-} \\frac{\\phi_-}{\\( 1 - self.phi_p - self.phi_a - self.phi_b )}`
+        :param f_a: math:`h_+ \\frac{\\upsilon_w}{\\upsilon_+} \\frac{\\phi_+}{\\(1 - self.phi_p - self.phi_+ - self.phi_-)}`
+        :param f_b: math:`h_- \\frac{\\upsilon_w}{\\upsilon_-} \\frac{\\phi_-}{\\(1 - self.phi_p - self.phi_+ - self.phi_-)}`
         :param chi_p: a Flory Huggins parameter with the temperature dependence:math:`\\chi_p=A_p+\\frac{B_p}{T}`
         :param chi_e: a Flory Huggins parameter between polymer and electrolytes
         :param df_w: free energy change upon formation of hydrogen bond in water (in :math:`k_BT` units)
@@ -43,6 +43,8 @@ class PolymerSolutionSalts(object):
         :param df_b: free energy change upon dissociated salt (in :math:`k_BT` units)
         :param h_a: number of water molecules forming the hydration shell of the + ions
         :param h_b: number of water molecules forming the hydration shell of the - ions
+        :param bjerrum_object: object of water class
+        :param b_fac: B_factors for the different species in units of ion_size
         """
 
         self.phi_p = phi_p
@@ -66,21 +68,46 @@ class PolymerSolutionSalts(object):
         self.x = x
         self.p = p
 
-    def free(self, c, ion_size, phi_w, phi_1):
+        self.bjerrum_object = bjerrum_object
+        self.b_fac = b_fac
+
+        self.phi_w = 1 - self.phi_p - self.phi_a - self.phi_b
+        self.phi_1 = self.phi_a + self.phi_b
+
+        self.c = np.array([self.f_a / self.h_a * 1000 / 18, self.f_b / self.h_b * 1000 / 18])
+
+    def dh_free(self, ion_size):
+        """
+        Debye Huckel Free energy according to :cite:`Levin1996`
+
+        :param ion_size: Ion diameter (in Angstrom)
+        :return: excess free energy per unit volume in units of :math:`\\frac{k_BT}{a^3}` (float)
+        """
+
+        dhfree = dh.DebyeHuckel(self.bjerrum_object).free_energy_db_excess(self.c, ion_size)
+        return dhfree
+
+    def hc_free(self, ion_size):
+        """
+        Excess free energy of the hard core
+
+        :param c: concentration of the different species mols/litre (M)
+        :param ion_size: ionic size (in Angstrom)
+        :return: excess free energy (float)
+        """
+
+        hcfree = hc.HardCore(b_fac=self.b_fac).free_energy_hc_excess(self.c, ion_size)
+        return hcfree
+
+    def free(self, ion_size):
         """
         Free energy of a polymer in solution :math:`\\frac{F \\upsilon}{V k_B T}=\\frac{f}{k_B T}`
         (per unit volume in :math:`k_B T` units)
 
-	:param c: concentration of ions (both + and -) mols/litre (M)
         :param ion_size: Ion diameter (in Angstrom)
-        :param phi_w: math:`1 - \\phi_p - \\phi_+ - \\phi_-`
-        :param phi_1: math:`\\phi_+ + \\phi_-`
         :return: value of free energy (float)
         """
-        self.c = c
-        self.ion_size = ion_size
-        self.phi_w = 1 - self.phi_p - self.phi_a - self.phi_b
-        self.phi_1 = self.phi_a + self.phi_b
+
         f_ref_11 = lg(self.u_p * self.phi_p / self.n, self.phi_p / (self.n * np.exp(1)))
         f_ref_12 = lg(self.u_a * self.phi_a, self.phi_a / np.exp(1))
         f_ref_13 = lg(self.u_b * self.phi_b, self.phi_b / np.exp(1))
@@ -94,10 +121,13 @@ class PolymerSolutionSalts(object):
         z_val = (1 - self.f_b - self. p - self.x * self.u_p * self.phi_p) / self.phi_w
         f_as_3 = 2 * self.phi_w * lg(z_val, z_val)
         f_as_4 = -2 * self.phi_w * lg(self.x * self.u_p * self.phi_p / self.phi_w + self.p, 2 * self.phi_w / np.exp(1))
-        f_as_5 = self.phi_w * ((np.log(np.math.factorial(self.h_a)) - self.df_a) * self.f_a + (np.log(np.math.factorial(self.h_b)) - self.df_b) * self.f_b)
+        f_as_5 = self.phi_w * ((np.log(gamma(self.h_a + 1)) - self.df_a) * self.f_a + (np.log(gamma(self.h_b + 1)) - self.df_b) * self.f_b)
         f_as_6 = self.phi_w * (lg(1 - self.f_a - self.f_b, 1 - self.f_a - self.f_b) + lg(1 - self.f_a, 1 - self.f_a) + lg(1 - self.f_b, 1 - self.f_b))
         f_as_7 = self.phi_w * (self.f_a + self.f_b) * np.log(self.phi_w / np.exp(1))
 
-        return f_ref_11 + f_ref_12 + f_ref_13 + f_ref_2 + f_int_1 + f_int_2 + f_as_1 + f_as_2 + f_as_3 + f_as_4 + f_as_5 + f_as_6 + f_as_7 + dh.DebyeHuckel(self.bjerrum_object).free_energy_db_excess(self.c, self.ion_size) + hc.HardCore(self.b_fac).free_energy_db_excess(self.c, self.ion_size)
+        f_dh = self.dh_free(ion_size)
+        f_hc = self.hc_free(ion_size)
+
+        return f_ref_11 + f_ref_12 + f_ref_13 + f_ref_2 + f_int_1 + f_int_2 + f_as_1 + f_as_2 + f_as_3 + f_as_4 + f_as_5 + f_as_6 + f_as_7 + f_dh + f_hc
 
 
