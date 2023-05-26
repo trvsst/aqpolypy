fixed the sign problem in the energies of mu_b as well, unit test passes

aqpolypy/free_energy_polymer/ElectrolyteSolution.py
@@ -408,7 +408,7 @@ class ElectrolyteSolution(object):
         s_bm = np.sum(in_p[12:15], axis=0)
 
         t_11 = np.log(in_p[15]*self.n_s)-s_bp*self.f_bp-s_bm*self.f_bm-self.f_bj
-        t_12 = in_p[10]*self.f_bp1+in_p[11]*self.f_bp2+in_p[13]*self.f_bm1+in_p[14]*self.f_bm2
+        t_12 = -in_p[10]*self.f_bp1-in_p[11]*self.f_bp2-in_p[13]*self.f_bm1-in_p[14]*self.f_bm2
         t_1 = t_11 + t_12
 
         t_2 = -in_p[9]*np.log(1-2*in_p[0]+in_p[1]-((1-in_p[15])*in_p[3]+in_p[15]*in_p[9])*self.r_h)

aqpolypy/unit_tests/electrolyte_solution_unit_test.py
@@ -240,7 +240,7 @@ class TestFreeEnergy(unittest.TestCase):
         self.in_p[15] = test_f_b
 
         comp_mu_b_salt_1 = el_mu.mu_sb_1(self.in_p)
-        vals_comp = [-34.0143746,  -29.88621445, -18.83486116, -13.3538229]
+        vals_comp = [-30.16770793, -26.03954779, -14.9881945,  -9.50715624]
         test_mu_b_salt_1 = np.allclose(comp_mu_b_salt_1, vals_comp, 0, 1e-6)
         self.assertTrue(test_mu_b_salt_1)
 