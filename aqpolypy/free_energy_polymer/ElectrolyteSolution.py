fixed the sign of a term log(M_+ M_-) unit tests pass

aqpolypy/free_energy_polymer/ElectrolyteSolution.py
@@ -218,7 +218,7 @@ class ElectrolyteSolution(object):
         t_14_4 = self.n_s*self.m_bm*in_p[15]*lg(s_bm/self.m_bm, s_bm/self.m_bm)
         t_14 = t_14_1 + t_14_2 + t_14_3 + t_14_4
 
-        t_15_1 = self.n_s*in_p[15]*np.log(self.m_m*self.m_p)-2*lg(in_p[0]*self.n_w, in_p[0]*self.n_w)
+        t_15_1 = -self.n_s*in_p[15]*np.log(self.m_m*self.m_p)-2*lg(in_p[0]*self.n_w, in_p[0]*self.n_w)
         t_15_2 = 2*self.n_s*(lg(1-in_p[15], 1-in_p[15])+lg(in_p[15], in_p[15]))-lg(self.n_s*in_p[15], self.n_s*in_p[15])
         t_15 = t_15_1+t_15_2
 
@@ -433,7 +433,7 @@ class ElectrolyteSolution(object):
         t_10_2 = self.m_bm*(lg(1-s_bm/self.m_bm, 1-s_bm/self.m_bm)+lg(s_bm/self.m_bm, s_bm/ self.m_bm))
         t_10 = t_10_1 + t_10_2
 
-        t_11 = np.log(self.m_m*self.m_p)-(s_bp+s_bm)*np.log(self.n_w)
+        t_11 = -np.log(self.m_m*self.m_p)-(s_bp+s_bm)*np.log(self.n_w)
         
         return t_1 + t_2 + t_3 + t_4 + t_5 + t_6 + t_7 + t_8 + t_9 + t_10 + t_11
 
@@ -450,7 +450,7 @@ class ElectrolyteSolution(object):
 
         t_0 = np.log(in_p[15]*self.n_s)-self.f_bj
         t_1 = self.m_bp*np.log(1-h_bp/self.m_bp)+self.m_bm*np.log(1-h_bm/self.m_bm)
-        t_2 = np.log(self.m_p*self.m_m)
+        t_2 = -np.log(self.m_p*self.m_m)
 
         return t_0+t_1+t_2
 
@@ -564,12 +564,12 @@ class ElectrolyteSolution(object):
         s_hbp = np.sum(in_p[9:12])
         s_hbm = np.sum(in_p[12:15])
 
-        k_0 = np.exp(self.f_bj)/(self.m_p*self.m_m)
+        k_0 = np.exp(self.f_bj)
 
         val_b = (1 - s_hbp / self.m_bp) ** (self.m_bp) * (1 - s_hbm / self.m_bm) ** (self.m_bm)
         val_t = (1-s_hp/self.m_p)**(self.m_p)*(1-s_hm/self.m_m)**(self.m_m)
 
-        k_h = val_t/val_b
+        k_h = self.m_p*self.m_m*val_t/val_b
 
         return k_0*k_h/self.delta_w
 

aqpolypy/unit_tests/electrolyte_solution_unit_test.py
@@ -119,7 +119,7 @@ class TestFreeEnergy(unittest.TestCase):
         in_p[15] = test_f_b
 
         comp_assoc = el_sol.f_assoc(in_p)
-        vals_comp = [-2.57388926, -2.72540227, -2.73953532, -2.72985608]
+        vals_comp = [-2.57388926, -2.72540227, -2.77903685, -2.7693576 ]
         test_a = np.allclose(comp_assoc, vals_comp, 0, 1e-6)
         self.assertTrue(test_a)
 
@@ -257,7 +257,7 @@ class TestFreeEnergy(unittest.TestCase):
         in_p[15] = test_f_b
 
         comp_mu_b_salt_1 = el_mu.mu_sb_ideal_assoc(in_p)
-        vals_comp = [-30.16590759, -25.94904943, -14.80637131, -9.32533305]
+        vals_comp = [-38.93524637, - 34.71838821, - 23.57571009, - 18.09467182]
         test_mu_b_salt_1 = np.allclose(comp_mu_b_salt_1, vals_comp, 0, 1e-6)
         self.assertTrue(test_mu_b_salt_1)
 
@@ -599,7 +599,7 @@ class TestFreeEnergy(unittest.TestCase):
         mu_c_ideal = delta_w*(el_p.f_ideal()-el_m.f_ideal())/(2*dm*c_fac)
         mu_f_idassoc = el_p.mu_sb_ideal_assoc(solb)
 
-        test_idassoc = np.allclose(mu_c_ideal + mu_c_assoc, mu_f_idassoc, 0, 5e-5)
+        test_idassoc = np.allclose(mu_c_ideal + mu_c_assoc, mu_f_idassoc, 0, 6e-5)
         self.assertTrue(test_idassoc)
 
         # water chemical potential